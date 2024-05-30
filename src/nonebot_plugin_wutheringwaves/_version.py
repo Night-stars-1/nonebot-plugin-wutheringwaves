368bb7e0382a0ccfd89afb17331937e1a18659ba
Night-stars-1 <nujj1042633805@gmail.com>
2024-05-31 00:14:14 +0800

    chore: 提升版本号

--- src/nonebot_plugin_wutheringwaves/_version.py
+++ src/nonebot_plugin_wutheringwaves/_version.py
@@ -1,1 +1,8 @@
+"""
+Author: Night-stars-1 nujj1042633805@gmail.com
+Date: 2024-05-25 00:18:28
+LastEditTime: 2024-05-31 00:13:52
+LastEditors: Night-stars-1 nujj1042633805@gmail.com
+"""
+
 __version__ = "0.1.1"
